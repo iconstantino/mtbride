422e61e85062f0d49532436b841d715862f2ab59
Ivan <icaivan11@gmail.com>
2020-01-09 18:11:30 -0800

    Initial Commit

---
+++ mtbride.api/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using mtbride.api.Models;
+
+namespace mtbride.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
